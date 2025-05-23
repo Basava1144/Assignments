66da4b265a7258d7e104b73c765f79b0cb07bc5e
Basava 1144 <basavaprasadpyati@outlook.com>
2025-05-23 18:46:35 +0530

    Assigment 1 completed related to datatypes

---
+++ src/assignments/Assignment1_Datatypes.java
@@ -0,0 +1,101 @@
+package assignments;
+
+public class Assignment1_Datatypes {
+
+	public static void main(String[] args) {
+	
+		/*1. Temperature of a city in degrees Celsius: 25.5*/
+		  float celsius =25.5f;
+		  System.out.println(celsius);
+		
+		/* 2. Whether a customer has placed an order: true or false*/
+         boolean order=true;
+		 System.out.println(order);
+		
+		/*3. Person's phone number: "123-456-7890"*/
+		 String phoneno="123-456-7890";
+		 System.out.println(phoneno);
+		 
+		/*4. Amount of money in a customer's bank account: 1000.50 */
+		 double balance =1000.50;
+		 System.out.println(balance);	
+		
+		/*5. Person's email address: "john.doe@example.com" */
+		  String emailid="john.doe@example.com";
+		  System.out.println(emailid);
+		
+		/*6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194 */
+		 double latitude =37.7749;
+		 double longitude=-122.4194;
+		 System.out.println( latitude + ", " + longitude);
+		
+		 
+		 
+		 /*7. Person's marital status: true or false */
+		 boolean ms=true;
+		 System.out.println(ms);
+		 
+		 
+		 /* 8. Person's occupation: "Software Engineer" */
+		 String occ="Software Engineer";
+		 System.out.println(occ);
+		 
+		 /* 9. Person's favourite colour: "Blue"  */
+		 String color ="Blue";
+		 System.out.println(color);
+		 
+		/* 10.Current year: 2023 */
+		 int currentyear= 2023;
+		 System.out.println(currentyear);
+		   
+		/*  11.Number of followers on a social media platform: 1,000,000 */
+		 int followers=100000;
+		 System.out.println(followers);
+		 
+		 
+
+        /* 12.Rating of a movie: 7.5  */
+		 float rating = 7.5f;
+	     System.out.println(rating);
+				 
+				 
+		 
+        /* 13.Person's blood type: 'A' */
+	     char bloodtype='A';
+	     System.out.println(bloodtype);
+	     
+        /*14.Title of a book: "To Kill a Mockingbird" */
+	     String title ="To Kill a Mockingbird";
+	     System.out.println(title);
+	     
+        /*15.Number of employees in a company: 500 */
+	     short employee =500;
+	     System.out.println(employee);
+	     
+        /* 16.Time of an event: 2:30 PM */
+	     String time ="2:30 PM";
+	     System.out.println(time);	
+	    
+       /*17.Name of a country: "United States" */
+         String country = "United States";
+	     System.out.println(country);
+	    
+       /*18.Person's eye color: "Brown" */
+        String eyecolor ="Brown";
+	    System.out.println(eyecolor);
+    
+      /*19.Person's birthplace: "New York City" */
+        String birthplace ="New York City";
+	    System.out.println(birthplace);
+	    
+	    
+     /*20.Distance between two cities: 200.5 */
+	    float distance=200.5f;
+	    System.out.println(distance);
+		
+		
+		 
+		 
+	}
+
+}
